420dbe351ab2fb17101335acbc7f65b12e0dcc4d
Miro Dietiker, MD Systems <miro.dietiker@md-systems.ch>
2018-11-08 04:04:24 +0100

    Fix notices on lots, support product deletion.

--- views/menu-stock-view.php
+++ views/menu-stock-view.php
@@ -4,12 +4,18 @@ global $wpdb, $wooyellowcube;
 
 $product_id = htmlspecialchars($_GET['id']);
 
-// Check if they is lots
+$product = wc_get_product((int)$product_id);
+if (!$product) {
+    echo '<h1>Product '.$product_id.' inexisting</h1>';
+    exit();
+}
+
+
+// Check if there are lots.
 $yellowcube_products_lots = $wpdb->get_results('SELECT * FROM wooyellowcube_stock_lots WHERE id_product=\''.$product_id.'\'');
 
-$product = new WC_Product((int)$product_id);
 
-echo '<h1>'.$product->post->post_title.'</h1>';
+echo '<h1>'.$product->get_title().'</h1>';
 echo '<p><a href="admin.php?page=wooyellowcube-stock">'.__('Go back to stock', 'wooyellowcube').'</a></p>';
 
 if(count($yellowcube_products_lots)) {
@@ -32,6 +38,6 @@ if(count($yellowcube_products_lots)) {
     echo '</table>';
 }else{
 
-    echo '<p>'.__('There is no lots on this product', 'wooyellowcube').'</p>';
+    echo '<p>'.__('There are no lots on this product.', 'wooyellowcube').'</p>';
 
 }
