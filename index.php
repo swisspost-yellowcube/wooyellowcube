dee9cd570b432107c07f34db035d628fc1091052
Miro Dietiker, MD Systems <miro.dietiker@md-systems.ch>
2019-04-02 14:11:42 +0200

    Increase version to 2.5.5

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooYellowCube
  * Plugin URI: http://www.yellowcube.ch
  * Description: WooCommerce synchronization with YellowCube
- * Version: 2.5.4
+ * Version: 2.5.5
  *
  * for any support please contact yellowcube.ch
  */
